a539368c74dcb3be7a8abb47759b2bba2e13af23
Park Changryum <myanjini@gmail.com>
2023-11-17 16:30:53 +0900

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd /var/www/html
+
 # sudo mkdir build
 
-# echo ">>> install node modules ***********************"
+# echo ">>> install node modules *********************"
 # npm install
 
-# echo ">>> build project"
+# echo ">>> build project ****************************"
 # npm run build
 
 echo ">>> copy build results to web root directory ***"
@@ -14,7 +15,7 @@ mv -f ./build/* ./
 
 echo ">>> remove template files & directories ********"
 # rm -rf build src scripts
-rm -rf scripts appspec.yml
+rm -rf scripts build appspec.yml
 
-echo '>>> change owner to ubuntu *********************'
+echo ">>> change owner to ubuntu *********************"
 chown -R ubuntu /var/www/html
